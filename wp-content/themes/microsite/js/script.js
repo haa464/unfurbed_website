79d6a168c1b9fab234817435eba726428f3cb808
unknown <you@example.com>
2024-11-11 09:08:18 +0530

    deploy the wordpress site

---
+++ wp-content/themes/microsite/js/script.js
@@ -0,0 +1,23 @@
+(function($) {
+
+  "use strict";
+  //   //declare variable 
+  //   const body =  document.querySelector("body"),
+  //   nav = document.querySelector(".menu-class"),
+  //   sidebarOpen = document.querySelector(".menu-button"),
+  //   siderbarClose = document.querySelector(".close-icon");
+
+  //   // menu for the mobile version
+  //   // toggle menu
+  //   sidebarOpen.addEventListener("click", () => {
+  //         nav.classList.add("active");
+  //   });
+
+  //   siderbarClose.addEventListener("click", () => {
+  //     nav.classList.remove("active");
+  //   });
+
+  
+
+})(jQuery);
+
